82b79d7e1e50ab6d2992845cf78e7a494ae27b1c
ColinRoxs <colin.mcp1029@gmail.com>
2026-05-01 17:21:58 -0400

    0.0.0.2

---
+++ Source/CyberGunner/FPSGameMode.cpp
@@ -0,0 +1,14 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSGameMode.h"
+
+void AFPSGameMode::StartPlay()
+{
+	Super::StartPlay();
+
+	check(GEngine != nullptr);
+
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Yellow, TEXT("Hello World, The game has started! Hybrid code worked!"));
+}
+
