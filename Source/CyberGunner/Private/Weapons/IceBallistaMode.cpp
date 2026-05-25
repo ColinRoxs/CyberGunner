6bdb239cb5d1059848b98b408e2bbb8c41d09558
ColinRoxs <colin.mcp1029@gmail.com>
2026-05-24 23:57:17 -0400

    0.0.0.16
    Added Ice Ballista fire mode. Needs more code work to get the widening projectile functionality, but for now it works.

--- Source/CyberGunner/Private/Weapons/IceBallistaMode.cpp
+++ Source/CyberGunner/Private/Weapons/IceBallistaMode.cpp
@@ -6,17 +6,38 @@
 
 void UIceBallistaMode::StartFire_Implementation(AFPSCharacter* Character)
 {
-	FVector MuzzleLocation;
-	FRotator MuzzleRotation;
-	GetMuzzleLocationAndRotation(Character, MuzzleLocation, MuzzleRotation);
+	bIsCharging = true;
+	CurrentChargeTime = 0.0f;
 
-	UE_LOG(LogTemp, Warning, TEXT("Ice Ballista Charging")); //temp log
-	SpawnDebugProjectile(Character, MuzzleLocation, MuzzleRotation);
-
+	UE_LOG(LogTemp, Warning, TEXT("Ice Ballista Charging Started")); //temp log
 }
 
 void UIceBallistaMode::TickFire_Implementation(AFPSCharacter* Character, float DeltaTime)
-{}
+{
+	if (!bIsCharging) return;
+
+	CurrentChargeTime += DeltaTime;
+	CurrentChargeTime = FMath::Clamp(CurrentChargeTime, 0.0f, MaxChargeTime);
+}
 
 void UIceBallistaMode::StopFire_Implementation(AFPSCharacter * Character)
-{}
+{
+	if (!bIsCharging) return;
+	bIsCharging = false;
+
+	const float ChargeRatio = CurrentChargeTime / MaxChargeTime;
+	const float FinalScale = FMath::Lerp(MinProjectileScale, MaxProjectileScale, ChargeRatio);
+
+	FVector MuzzleLocation;
+	FRotator MuzzleRotation;
+	GetMuzzleLocationAndRotation(Character, MuzzleLocation, MuzzleRotation);
+	
+	UE_LOG(LogTemp, Warning, TEXT("Ice Ballista Charging")); //temp log
+
+	SpawnDebugProjectile(Character, MuzzleLocation, MuzzleRotation);
+
+
+	CurrentChargeTime = 0.0f;
+
+
+}
